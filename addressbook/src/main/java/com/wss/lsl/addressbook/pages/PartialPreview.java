b2e2fcabfc88f514e4a1229170cbe742500544ca
weishengshui <657620636@qq.com>
2013-05-30 18:06:05 +0800

    xxx

--- addressbook/src/main/java/com/wss/lsl/addressbook/pages/PartialPreview.java
+++ addressbook/src/main/java/com/wss/lsl/addressbook/pages/PartialPreview.java
@@ -4,10 +4,9 @@ import org.apache.tapestry5.Asset;
 import org.apache.tapestry5.annotations.AfterRender;
 import org.apache.tapestry5.annotations.Environmental;
 import org.apache.tapestry5.annotations.Path;
+import org.apache.tapestry5.annotations.Property;
 import org.apache.tapestry5.ioc.annotations.Inject;
 import org.apache.tapestry5.services.javascript.JavaScriptSupport;
-import org.apache.tapestry5.services.javascript.StylesheetLink;
-import org.apache.tapestry5.services.javascript.StylesheetOptions;
 
 /**
  * 局部预览
@@ -18,21 +17,20 @@ import org.apache.tapestry5.services.javascript.StylesheetOptions;
 public class PartialPreview {
 
 	@Inject
-	@Path("context:css/ps/base.css")
-	private Asset baseCss;
-
-	@Inject
 	@Path("context:css/ps/pshow.css")
 	private Asset pshowCss;
+	@Property
+	private final String pshowJS = "js/ps/pshow.js";
 
 	@Environmental
 	private JavaScriptSupport javaScriptSupport;
 
 	@AfterRender
 	void loadCssFile() {
-		javaScriptSupport.importStylesheet(new StylesheetLink(baseCss,
-				new StylesheetOptions("all")));
-		javaScriptSupport.importStylesheet(new StylesheetLink(pshowCss,
-				new StylesheetOptions("all")));
+		javaScriptSupport.importStylesheet(pshowCss);
+//		javaScriptSupport.importStylesheet(new StylesheetLink(baseCss,
+//				new StylesheetOptions("all")));
+//		javaScriptSupport.importStylesheet(new StylesheetLink(pshowCss,
+//				new StylesheetOptions("all")));
 	}
 }
